428fd2e41e73a9683f6c9d80e7e77dd95c4a1282
PANxxJ <ps789875@gmail.com>
2023-10-02 12:32:38 +0530

    change db

--- invoicely_django/settings.py
+++ invoicely_django/settings.py
@@ -87,26 +87,26 @@ WSGI_APPLICATION = 'invoicely_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'nable_ess',
-        'NAME': 'invoicely_django',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        # 'PASSWORD': 'pos',
-        # 'HOST': '34.93.184.212',
-        'HOST': '127.0.0.1',
-        'PORT': '5432', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         # 'NAME': 'nable_ess',
+#         'NAME': 'invoicely_django',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         # 'PASSWORD': 'pos',
+#         # 'HOST': '34.93.184.212',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432', 
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
